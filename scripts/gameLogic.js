362b5f8c1ee62a276dfbbb70db223bb7e483235f
paulauskas-linas <paulauskas.linas@gmail.com>
2022-04-26 07:35:54 +0300

    add font, html markup, css styling, working js code

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,24 +1,47 @@
+
 const playerButtons = document.querySelectorAll(".player-button");
-const computerChoice = document.querySelector(".computer-choice");
-const roundCounter = document.querySelector(".round-counter");
 const computerScoreBoard = document.querySelector(".computer-score");
 const playerScoreBoard = document.querySelector(".player-score");
-const roundComments = document.querySelector(".round-comments");
-const gameComments = document.querySelector(".game-comments");
+const logList = document.querySelector(".log-ul");
+const playAgainButton = document.querySelector(".play-again-button");
 const choices=["rock", "paper", "scissors"];
 
+let computerSelection = "paper";
+let playerSelection = "paper";
 let playerScore = 0;
 let computerScore = 0;
-let roundCount = 0;
+let roundResult = "";
 
 playerButtons.forEach(button => {
-    const playerSelection = button.getAttribute('id').toLowerCase();
     button.addEventListener('click', () => {
-        computerChoice.textContent = `${computerPlay()}`;
-        playRound(playerSelection, computerChoice.textContent);
-        roundCounter.textContent = roundCount;
+        playerSelection = button.getAttribute('id').toLowerCase();
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        colorComputerChoice();
     })
 });
+playAgainButton.addEventListener('click', () => {
+    resetGame()
+});
+
+function colorComputerChoice(){
+    const rockAI = document.getElementById("rockAI");
+    const paperAI = document.getElementById("paperAI");
+    const scissorsAI = document.getElementById("scissorsAI");
+    if (computerSelection === "rock") {
+        rockAI.classList.add("chosen-button");
+        paperAI.classList.remove("chosen-button");
+        scissorsAI.classList.remove("chosen-button");
+    } else if (computerSelection === "paper") {
+        paperAI.classList.add("chosen-button");
+        rockAI.classList.remove("chosen-button");
+        scissorsAI.classList.remove("chosen-button");
+    } else if (computerSelection === "scissors") {
+        scissorsAI.classList.add("chosen-button");
+        rockAI.classList.remove("chosen-button");
+        paperAI.classList.remove("chosen-button");
+    }
+}
 
 function computerPlay() {
     return choices[Math.floor(choices.length*Math.random())];
@@ -30,51 +53,58 @@ function playRound (playerSelection, computerSelection) {
         (playerSelection == "paper" && computerSelection == "paper") ||
         (playerSelection == "scissors" && computerSelection == "scissors")
         ) {
-            roundComments.textContent =`It's a tie! You both have chosen ${playerSelection}`;
-            roundCount++;
+            roundResult = "tie";
         } else if (
             (playerSelection == "rock" && computerSelection == "paper") || 
             (playerSelection == "paper" && computerSelection == "scissors") ||
             (playerSelection == "scissors" && computerSelection == "rock")
             ) {
-                roundComments.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+                roundResult = "lost";
                 computerScore++;
-                roundCount++;
-                computerScoreBoard.textContent = `${computerScore}`
-                playerScoreBoard.textContent = `${playerScore}`;
-                checkWinner();
             } else if (
                 (playerSelection == "rock" && computerSelection == "scissors") ||
                 (playerSelection == "paper" && computerSelection == "rock") ||
                 (playerSelection == "scissors" && computerSelection == "paper")
                 ) {
-                    roundComments.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+                    roundResult = "win";
                     playerScore++;
-                    roundCount++;
-                    computerScoreBoard.textContent = `${computerScore}`
-                    playerScoreBoard.textContent = `${playerScore}`;
-                    checkWinner();
                 }
+    updateLog();
+    updateScore();
+    checkWinner();
 };
-
+function updateLog() {
+    if (roundResult == 'tie') {
+        const logListItem = document.createElement("li");
+        logListItem.appendChild(document.createTextNode(`It's a tie! You both have chosen ${playerSelection}`));
+        logListItem.setAttribute("class", "log-li")
+        logList.insertBefore(logListItem, logList.firstChild);
+    } else if (roundResult == 'lost') {
+        const logListItem = document.createElement("li");
+        logListItem.appendChild(document.createTextNode(`You lose! Enemy's ${computerSelection} beats ${playerSelection}`));
+        logListItem.setAttribute("class", "log-li");
+        logList.insertBefore(logListItem, logList.firstChild);
+    } else if (roundResult == 'win') {
+        const logListItem = document.createElement("li");
+        logListItem.appendChild(document.createTextNode(`You win! Your ${playerSelection} beats ${computerSelection}`));
+        logListItem.setAttribute("class", "log-li");
+        logList.insertBefore(logListItem, logList.firstChild);
+    }
+}
+function updateScore() {
+    computerScoreBoard.textContent = `${computerScore}`
+    playerScoreBoard.textContent = `${playerScore}`
+}
 function checkWinner(){
     if (playerScore === 5) {
-    alert(`You won the game!`); 
+        alert(`You won the game!`); 
         resetGame();
     } else if (computerScore === 5) {
-        alert(`You lost the game. Computer wins.`);
+        alert(`You lost the game. Enemy wins.`);
         resetGame();
     }
 }
 
 function resetGame(){
-    playerScore = 0;
-    computerScore = 0;
-    roundCount = 0;
-    computerScoreBoard.textContent = `${computerScore}`
-    playerScoreBoard.textContent = `${playerScore}`;
-    roundCounter.textContent = roundCount;
+    window.location.reload();
 }
-
-
-// playagain window with refresh page
